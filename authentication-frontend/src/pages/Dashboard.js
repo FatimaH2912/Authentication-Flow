226f6659a26a03b9e404e4359411d5011e89076c
FatimaH2912 <fashroon111@gmail.com>
2026-07-29 14:53:52 +0300

    Implemented Logout Functionality

--- authentication-frontend/src/pages/Dashboard.js
+++ authentication-frontend/src/pages/Dashboard.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import api from "../services/api";
 
 function Dashboard() {
   const [user, setUser] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function fetchProfile() {
@@ -16,6 +18,7 @@ function Dashboard() {
         });
 
         setUser(response.data);
+
       } catch (error) {
         console.log(error);
       }
@@ -23,6 +26,28 @@ function Dashboard() {
 
     fetchProfile();
   }, []);
+
+  async function handleLogout() {
+    try {
+      const token = localStorage.getItem("token");
+
+      await api.post(
+        "/auth/logout",
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+    } catch (error) {
+      console.log(error);
+    }
+
+    localStorage.removeItem("token");
+    navigate("/login");
+  }
 
   return (
     <div style={{ padding: "40px" }}>
@@ -33,6 +58,10 @@ function Dashboard() {
           <p><strong>ID:</strong> {user.id}</p>
           <p><strong>Email:</strong> {user.email}</p>
           <p><strong>Created At:</strong> {user.created_at}</p>
+
+          <button onClick={handleLogout}>
+            Logout
+          </button>
         </>
       ) : (
         <p>Loading...</p>
